7072895ecd75acc1aac42b2d7e47cdd8811a3d8f
Srilakshmi Malempati <smalempa@uab.edu>
2023-08-07 11:22:25 -0500

    updated timeout issue fix

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
-const aws = require('aws-sdk');
-const multer = require('multer');
-const nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 const randomUUID = uuidv4();
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+const aws = require('aws-sdk');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
 app.use(bodyParser.json());
 app.use(cors());
 const pool = require('./db'); 
-const accessKey = {
-  accessKeyId: 'AKIAUPCKTHUGWACQ2HXD',   
-  secretAccessKey: '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', 
-  region: 'us-east-2',        
-}
-aws.config.update(accessKey);
+aws.config.update({
+   accessKeyId: "AKIAUPCKTHUGWACQ2HXD",
+        secretAccessKey: "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", 
+  region: 'us-east-2',             
+});
 const sns = new aws.SNS();
 const lambda = new aws.Lambda();
 const upload = multer();
-function file_delete_if_clicks_count_exceed(url){
+function delete_file_if_file_exceeds(url){
   const s3 = new aws.S3();
 
 const filePathUrl = url;
@@ -29,6 +28,7 @@ const filePathUrl = url;
 const urlParts = filePathUrl.split('/');
 const bucketName = urlParts[2];
 const objectKey = urlParts[urlParts.length -1];
+console.log('vv', bucketName, objectKey)
 // Define the parameters for the headObject operation to check file existence
 const headParams = {
   Bucket: 'smalempabucket',
@@ -68,8 +68,8 @@ const getSubscribedUsers = async (topicArn) => {
     const snsClient = new SNSClient({
       region: "us-east-2", // Replace with your desired AWS region
       credentials: {
-         accessKeyId: 'AKIAUPCKTHUGWACQ2HXD',   
-  secretAccessKey: '7s1lIBsPgvIh+eXPUwD81ADKzehEbNTJcbhFM+lW', 
+   accessKeyId: "AKIAUPCKTHUGWACQ2HXD",
+        secretAccessKey: "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",
       },
     });
 
@@ -82,12 +82,11 @@ const getSubscribedUsers = async (topicArn) => {
     return [];
   }
 };
-const updateCount = async (id, url) => {
-  const selectSql = 'SELECT emails, id, count FROM fileclickcount WHERE id = ?';
-  console.log(id, 'count')
+const updateClick = async (id, url) => {
+  const selectSql = 'SELECT emails, id, clicks FROM file_clicks WHERE id = ?';
+
   try {
     const [rows] = await pool.query(selectSql, [id]);
-    console.log(rows, 'rowsrowsrows')
     if (rows.length === 0) {
       console.log('No data found for the given ID.');
       return false;
@@ -95,17 +94,16 @@ const updateCount = async (id, url) => {
 
     const row = rows[0];
     const emailCount = row.emails.split(',').length;
-    if (emailCount > row.count) {
-      const updateSql = 'UPDATE fileclickcount SET count = count + 1 WHERE id = ?';
+    if (emailCount > row.clicks) {
+      const updateSql = 'UPDATE file_clicks SET clicks = clicks + 1 WHERE id = ?';
       await pool.query(updateSql, [id]);
       console.log('Click updated successfully.');
       return true;
     } else {
-      file_delete_if_clicks_count_exceed(url)
+      delete_file_if_file_exceeds(url)
       console.log('Cannot update click. Click limit reached.');
       return false;
     }
-    return 
   } catch (error) {
     console.error('Error while updating click:', error);
     return false;
@@ -136,33 +134,6 @@ const insertItemsToTable=async (tablename,values)=>{
   }
   
 }
-const insertingDataToTables=async (tablename,values)=>{
-  let sql="";
-  if (tablename == "fileuploaddetails") {
-      sql = 'INSERT INTO fileuploaddetails (emails, filename,fileuploadeddate,fileurl,id) VALUES (?, ?, ?,?,?)';
-    pool.query(sql, [values.emails, values.filename, values.fileuploadeddate, values.fileurl, values.id], (err, results) => {
-      if (err) {
-           console.log(145, 'count')
-          return console.error('Encountered error while inserting data:', err);
-      } else {
-         console.log(148, 'count') 
-            return console.log('Successfully data got inserted:', results);
-          }
-       }) 
-    } 
-      else if(tablename=="fileclickcount"){
-      sql = 'INSERT INTO fileclickcount (emails, fileurl, count, id) VALUES (?, ?, ?,?)';
-      pool.query(sql, [values.emails, values.fileurl,0, values.id], (err, results) => {
-        if (err) {
-          return console.error('Encountered error while inserting data:', err);
-          } else {
-            return console.log('Successfully data got inserted:', results);
-          }
-       }) 
-  }
-  
-}
-
 
 app.post('/api/upload', upload.single('file') , async (req, res) => {
  try {
@@ -184,31 +155,31 @@ app.post('/api/upload', upload.single('file') , async (req, res) => {
   let randomId = new Date().getDate().toString(36)+new Date().getTime().toString(36)
     const topicArn = 'arn:aws:sns:us-east-2:307246611725:smalempatopic';
     const userEmails = await getSubscribedUsers(topicArn);
-     const fileUrl = JSON.parse(response.body).fileUrl; 
+      const fileUrl = JSON.parse(response.body).fileUrl; 
     console.log('==========')
-     insertingDataToTables('fileuploaddetails', { emails:userEmails.join(','), filename:req.file.originalname, fileuploadeddate:new Date(), fileurl: JSON.parse(response.body).fileUrl,id:randomId})
-    insertingDataToTables('fileclickcount', {emails:userEmails.join(','),  fileurl: JSON.parse(response.body).fileUrl, count: 0,id:randomId})
+    insertItemsToTable('file_upload_list', {id:randomId, emails:userEmails.join(','), file_name:req.file.originalname, uploaded_date:new Date(), file_url: JSON.parse(response.body).fileUrl})
+    insertItemsToTable('file_clicks', {id:randomId, emails:userEmails.join(','),  file_url: JSON.parse(response.body).fileUrl, clicks: 0})
     const snsClient = new SNSClient({
       region: "us-east-2", 
       credentials: {
-         accessKeyId: 'AKIAUPCKTHUGWACQ2HXD',   
-  secretAccessKey: '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', 
+     accessKeyId: "AKIAUPCKTHUGWACQ2HXD",
+        secretAccessKey: "7s1lIBsPgvIh+eXPUwD81ADKzehEbNTJcbhFM+lW",
       },
     });
 
-    const message = 'Please click on the link provided to download your file:'+ `http://localhost:4200/fetch/id=${randomId}_url${fileUrl.split(".com")[1].replace(/^\/+/, '')}`;
+    const message = 'Your file is ready to download:'+ `http://localhost:4200/fetch/id=${randomId}_url${fileUrl.split(".com")[1].replace(/^\/+/, '')}`;
     const snsPublishParams = {
       TopicArn: topicArn,
       Message: message,
     };
     console.log(fileUrl.split(".com"))
     await snsClient.send(new PublishCommand(snsPublishParams));
-    console.log('A file link has been sent to the subscribed users successfully.');
+    console.log('File link sent to the subscribed users successfully.');
    
-    return res.status(200).json({ success: true, message: 'File has been uploaded to S3 successfully' });
+    res.status(200).json({ success: true, message: 'File uploaded to S3 successfully' });
   } catch (error) {
     console.error('Upload failed:', error);
-    return res.status(500).json({ success: false, message: 'Upload failed' });
+    res.status(500).json({ success: false, message: 'Upload failed' });
   }
 });
 
@@ -225,7 +196,7 @@ const subscribeEmailToTopic = async (topicArn, email) => {
     const data = await sns.subscribe(params).promise();
     return data.ConfirmationUrl; // Get the Confirmation URL from the response
   } catch (error) {
-    console.error('Error has been encountered while subscribing email:', error);
+    console.error('Error subscribing email:', error);
     throw error;
   }
 };
@@ -239,7 +210,7 @@ const createTopic = async (topicName) => {
     console.log('hg', data)
     return data.TopicArn;
   } catch (error) {
-    console.error('Error has been encountered while creating topic:', error);
+    console.error('Error creating topic:', error);
     throw error;
   }
 };
@@ -249,7 +220,7 @@ app.post('/api/subscriptions/send', async(req, res) => {
 
   try {
     const topicArn = await createTopic('smalempatopic');
-    console.log('Created topic:', topicArn);
+    console.log('Topic created:', topicArn);
 
     const subscriptionPromises = emails.map(async (email) => {
       const confirmationUrl = await subscribeEmailToTopic(topicArn, email);
@@ -262,13 +233,13 @@ app.post('/api/subscriptions/send', async(req, res) => {
     res.status(200).json({
       status: 200,
       data: confirmationUrls,
-      message: 'The request mail to subscribe has been sent',
+      message: 'Mail Request Sent Successfully',
     });
   } catch (error) {
-    console.error('Error has been encountered in subscription process:', error);
+    console.error('Error in subscription process:', error);
     res.status(500).json({
       status: 500,
-      error: 'Error has been encountered in subscription process',
+      error: 'Error in subscription process',
     });
   }
 });
@@ -276,7 +247,7 @@ app.post('/api/subscriptions/send', async(req, res) => {
 
 app.post('/api/count', async(req, res) => {
   try{
- if(await updateCount(req.body.userId, req.body.url)){
+ if(await updateClick(req.body.userId, req.body.url)){
     res.status(200).json({status:200, data: true})
   } else{
     res.status(200).json({status:200, data: false})
@@ -288,7 +259,7 @@ app.post('/api/count', async(req, res) => {
   
 })
 
-const PORT = 4600; 
+const PORT = 3000; 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
